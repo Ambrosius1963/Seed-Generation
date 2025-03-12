ef8d7197e0dc450020195a60f8b96e845b4cc8a2
Andrew McKenzie <heldiggris7@gmail.com>
2025-03-12 12:32:57 -0600

    createdXYZValues

--- Program.cs
+++ Program.cs
@@ -7,27 +7,55 @@ class Program
     {
         // Generate a seed using the current date and time
         string seed = GenerateSeed();
-        Console.WriteLine($"\nGenerated Seed (from Date): {seed}");
+        Console.WriteLine($"\n\nGenerated Seed (from Date): {seed}");
 
         // Example usage of decisionMaker
-        int randomInt = decisionMaker<int>(seed, 100.0, 4);
+        // int randomInt = decisionMaker<int>(seed, 100.0, 4);
+        
+        // call desicionMaker 3 times to get 3 random numbers
+        List<int> randomInts = decisionMaker<int>(seed, 100.0, 4);
+        // Access the three numbers individually
+        int xValueDate = randomInts[0];
+        int yValueDate = randomInts[1];
+        int ZValueDate = randomInts[2];
+
         //double randomDouble = decisionMaker<double>(seed, 50.0, 6);
         //char randomChar = decisionMaker<char>(seed, 26.0, 2);
 
-        Console.WriteLine($"Random Int: {randomInt}");
+        // Console.WriteLine($"Random Int: {randomInts}");
+        Console.WriteLine("\n~Random Ints from Date~");
+        Console.WriteLine($"x: {xValueDate}");
+        Console.WriteLine($"y: {yValueDate}");
+        Console.WriteLine($"z: {ZValueDate}\n");
+
         //Console.WriteLine($"Random Double: {randomDouble}");
         //Console.WriteLine($"Random Char: {randomChar}");
         
         // Example usage of GenerateSeedFromText
-        string inputText = "Chicken Nuggets";
+        // string inputText = "Chicken Nuggets rule the World!!"; // Chicken Nuggets rule the World!!: 45694173826542680987491077817199
+        Console.Write("Please enter a seed: ");
+        string inputText = Console.ReadLine();
+
         string seedFromWord = GenerateSeedFromText(inputText);
+        Console.WriteLine($"Text: {inputText}");
         Console.WriteLine($"Generated Seed from text: {seedFromWord}"); 
-        int randomInt2 = decisionMaker<int>(seedFromWord, 100.0, 4);
-        Console.WriteLine($"Random Int: {randomInt2}");
+        // int randomInt2 = decisionMaker<int>(seedFromWord, 100.0, 4);
+
+        List<int> randomIntsFromWord = decisionMaker<int>(seedFromWord, 100.0, 4);
+        // int randomInt2 = randomIntsFromWord[0];
+        int xValueWord = randomIntsFromWord[0];
+        int yValueWord = randomIntsFromWord[1];
+        int zValueWord = randomIntsFromWord[2];
+
+        // Console.WriteLine($"Random Int: {randomInt2}");
+        Console.WriteLine("\n~Random Ints from Word~");
+        Console.WriteLine($"x: {xValueWord}");
+        Console.WriteLine($"y: {yValueWord}");
+        Console.WriteLine($"z: {zValueWord}\n");
         
                                                                         
-        string NewString = inputText.Insert(5, "T");
-        Console.WriteLine(NewString); // Output: HelloTWorld
+        // string NewString = inputText.Insert(5, "T");
+        // Console.WriteLine(NewString);
     }
     
     // generate random seed from the Date and Time
@@ -108,24 +136,32 @@ class Program
         return string.Join("", seedNumbers);
     }
 
-    static T decisionMaker<T>(string seed, double maxValue, int rangeLength)
+    static List<T> decisionMaker<T>(string seed, double maxValue, int rangeLength)
     {
         string seedString = seed;
         int startIdx = seedString.Length - rangeLength;
+        // Console.Out.WriteLine($"Start Index: {startIdx}"); // Debugging
         if (startIdx < 0) 
             startIdx = 0;
 
         int numericSeed = int.Parse(seedString.Substring(startIdx, rangeLength));
         Random random = new Random(numericSeed);
 
-        object result = typeof(T) switch
-        {
-            Type t when t == typeof(int) => random.Next((int)maxValue),
-            //Type t when t == typeof(double) => random.NextDouble() * maxValue,
-            //Type t when t == typeof(char) => (char)('A' + random.Next((int)maxValue)),
-            _ => throw new NotSupportedException("Unsupported data type")
-        };
+        List<T> results = new List<T>();
 
-        return (T)result;
+        for (int i = 0; i < 3; i++)
+        {
+            object result = typeof(T) switch
+            {
+                Type t when t == typeof(int) => random.Next((int)maxValue),
+                // Type t when t == typeof(double) => random.NextDouble() * maxValue,
+                // Type t when t == typeof(char) => (char)('A' + random.Next((int)maxValue)),
+                _ => throw new NotSupportedException("Unsupported data type")
+            };
+            results.Add((T)result);
+        }
+
+        return results;
     }
+
 }
